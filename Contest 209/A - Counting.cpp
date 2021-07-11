bedaea5b44e25e1b9b78df37a050d9b2afc64450
bigeyes <33706689+leedohun@users.noreply.github.com>
2021-07-11 13:40:05 +0900

    Update A - Counting.cpp

--- Contest 209/A - Counting.cpp
+++ Contest 209/A - Counting.cpp
@@ -1,1 +1,26 @@
-
+#define _CRT_SECURE_NO_WARNINGS
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <climits>
+#include <bitset>
+#include <queue>
+ 
+typedef long long ll;
+ 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+ 
+    int a, b; cin >> a >> b;
+ 
+    if (a > b)
+        cout << 0;
+    else
+        cout << b - a + 1;
+}
